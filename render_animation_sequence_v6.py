99e8eb4fb42ac39e37acb2df347c854b1ec1fbde
Mikhail Lebedev <softdrawart@gmail.com>
2025-02-10 18:14:12 +0300

    Update render_animation_sequence_v6.py
    duplicate render, collection visibility store

--- render_animation_sequence_v6.py
+++ render_animation_sequence_v6.py
@@ -9,15 +9,76 @@ bl_info = {
 }
     
 import bpy
+import json
 
 '''FIX 1. currently default scene is None, if it will be called none it will be ambigous whether its a scene or not
         2.  current value '0' matches no enum in 'RENDER_Props', '', 'track_name
-        3. group in a way where scene '''
+        3. group in a way where scene 
+        4. Store Settings before changing them'''
 
 '''1. First we select the scene name
    2. then we select the view layer 
    3. then we select Armature Or Camera
    4. then we select Track of the Armature'''
+
+def get_collection_path(layer_collection, path=""):
+    """ Recursively build the full path for a collection. """
+    path = f"{path}/{layer_collection.collection.name}" if path else layer_collection.collection.name
+    paths = {path: layer_collection.exclude}
+    for child in layer_collection.children:
+        paths.update(get_collection_path(child, path))
+    return paths
+
+def set_collection_visibility(visibility_dict):
+    """ Apply stored visibility states using full paths. """
+    def restore_visibility(layer_collection, path=""):
+        path = f"{path}/{layer_collection.collection.name}" if path else layer_collection.collection.name
+        if path in visibility_dict:
+            layer_collection.exclude = visibility_dict[path]
+        for child in layer_collection.children:
+            restore_visibility(child, path)
+
+    restore_visibility(bpy.context.view_layer.layer_collection)
+
+class CLEAR_OT_collection_visibility(bpy.types.Operator):
+    """Clear collection visibility"""
+    bl_idname = "view3d.clear_collection_visibility"
+    bl_label = "Clear Collection Visibility"
+
+    index: bpy.props.IntProperty() #index of the render block
+
+    def execute(self, context):
+        property = bpy.data.workspaces[0].render_panel_props[self.index]
+        property.collection_visibility = ''
+        self.report({'INFO'}, "Collection visibility cleared")
+        return {'FINISHED'}
+    
+class STORE_OT_collection_visibility(bpy.types.Operator):
+    """Store collection visibility"""
+    bl_idname = "view3d.store_collection_visibility"
+    bl_label = "Store Collection Visibility"
+
+    index: bpy.props.IntProperty() #index of the render block
+
+    def execute(self, context):
+        property = bpy.data.workspaces[0].render_panel_props[self.index]
+        property.collection_visibility = json.dumps(get_collection_path(bpy.context.view_layer.layer_collection))
+        self.report({'INFO'}, "Collection visibility stored")
+        return {'FINISHED'}
+
+class RESTORE_OT_collection_visibility(bpy.types.Operator):
+    """Restore collection visibility"""
+    bl_idname = "view3d.restore_collection_visibility"
+    bl_label = "Restore Collection Visibility"
+
+    index: bpy.props.IntProperty() #index of the render block
+
+    def execute(self, context):
+        property = bpy.data.workspaces[0].render_panel_props[self.index]
+        visibility_dict = json.loads(property.collection_visibility)
+        set_collection_visibility(visibility_dict)
+        self.report({'INFO'}, "Collection visibility restored")
+        return {'FINISHED'}
 
 def update_scene_list(self, context):
     scene_items = [(sc.name, sc.name, '') for sc in bpy.data.scenes]
@@ -64,9 +125,10 @@ def update_time(self, context):
     track_index = arm_data.animation_data.nla_tracks.find(self.track_name) if arm_data and arm_data.animation_data and arm_data.animation_data.nla_tracks else None
     if track_index != None and track_index != -1:
         self.frame_start = int(bpy.data.objects[self.rig_name].animation_data.nla_tracks[self.track_name].strips[0].frame_start_ui)
-        self.frame_end = int(bpy.data.objects[self.rig_name].animation_data.nla_tracks[self.track_name].strips[0].frame_end_ui)
+        self.frame_end = int(bpy.data.objects[self.rig_name].animation_data.nla_tracks[self.track_name].strips[0].frame_end_ui) - 1 #minus one frame that is similar to the first frame
 
 def update_output_folder(self, context):
+    #format "{render\character_name\track_name\camera\}"
     #set output folder based on the context
     output = "//..\\render\\"
 
@@ -81,21 +143,52 @@ def update_output_folder(self, context):
         case _:
             output += f"{self.character_name}\\"
     #-track
+    track_separated = self.track_name.split('_')
     if self.track_name:
-        output += f"{self.track_name}\\"
+        track_separated = self.track_name.split('_')
+        cleaned_name = '_'.join([part for part in track_separated if part not in ['up', 'down']])
+        output += f"{cleaned_name}\\"
     #--camera
     if self.cam_name == 'NONE':
         scene_separated = self.scene_name.split('_')
         view_separated = self.view_name.split('_')
         
-        if 'up' in scene_separated or 'up' in view_separated:
+        if 'up' in scene_separated or 'up' in view_separated or 'up' in track_separated:
                 output += "up\\"
-        elif 'down' in scene_separated or 'down' in view_separated:
+        elif 'down' in scene_separated or 'down' in view_separated or 'down' in track_separated:
                 output += "down\\"
     else:
         output += f"{self.cam_name}\\"
         
     self.output_path = output
+
+def form_render_text(self, property):
+    #format "{character_name|track_name|camera}"
+    #set output folder based on the context
+    output = ""
+
+    #character
+    if property.character_name:
+        output += f"{property.character_name}"
+    #-track
+    track_separated = property.track_name.split('_')
+    if property.track_name:
+        track_separated = property.track_name.split('_')
+        cleaned_name = '_'.join([part for part in track_separated if part not in ['up', 'down']])
+        output += f"|{cleaned_name}"
+    #--camera
+    if property.cam_name == 'NONE':
+        scene_separated = property.scene_name.split('_')
+        view_separated = property.view_name.split('_')
+        
+        if 'up' in scene_separated or 'up' in view_separated or 'up' in track_separated:
+                output += "|up"
+        elif 'down' in scene_separated or 'down' in view_separated or 'down' in track_separated:
+                output += "|down"
+    else:
+        output += f"|{property.cam_name}"
+        
+    return output
 
 class RENDER_Props(bpy.types.PropertyGroup):
     
@@ -145,6 +238,9 @@ class RENDER_Props(bpy.types.PropertyGroup):
         default="//..\\render\\",
         maxlen=1024,
         subtype='DIR_PATH')
+    collection_visibility: bpy.props.StringProperty(
+        description="Stored Collections Exclude parametr"
+        )
 
 class RENDER_PT(bpy.types.Panel):
     bl_idname = 'RENDER_PT'
@@ -156,23 +252,30 @@ class RENDER_PT(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        
-        add_button= layout.row()
-        add_button.operator('render.add_render', text = "Add")
-
-        
         # Here you want to iterate over your properties and keep the index for the Delete button
         properties = bpy.data.workspaces[0].render_panel_props
         for i, prop in enumerate(properties):
             render_box = layout.box()
             header = render_box.row()
             header.prop(prop, "enabled")
-            header.label(text = f'Render {i + 1}')
-            header.label(text = properties[i].output_path)
+
+            render_text = form_render_text(self, prop)
+            header.label(text = f'Render {i + 1}|{render_text}')
+
             header.prop(prop, "folded", icon='DISCLOSURE_TRI_DOWN' if prop.folded else 'DISCLOSURE_TRI_RIGHT')
+
             if prop.folded:
                 col = render_box.column()
                 col.prop(prop, "character_name")
+                col.label(text = "Store collection visibility:")
+                row = col.row()
+                row.operator(STORE_OT_collection_visibility.bl_idname, text = "Store").index=i
+                
+                if prop.collection_visibility and prop.collection_visibility != '':
+                    row.operator(RESTORE_OT_collection_visibility.bl_idname, text = "Re-Store").index=i
+                    row.alert = True
+                    row.operator(CLEAR_OT_collection_visibility.bl_idname, text = "- remove -").index=i
+
                 col.prop(prop, "scene_name")
                 col.prop(prop, "view_name")
                 col.prop(prop, "rig_name")
@@ -184,10 +287,14 @@ class RENDER_PT(bpy.types.Panel):
                 col.prop(prop, "output_path")
                 #button delete
                 col.alert = True
-                col.operator('render.delete_render', text = "-").index=i
+                row = col.row()
+                row.operator(RENDER_OT_DeleteRender.bl_idname, text = "- remove -").index=i
+                row.alert = False
+                row.operator(RENDER_OT_DuplicateRender.bl_idname, text = "duplicate").index=i #duplicate
         #render button
-        render_button = layout.row()
-        render_button.operator('render.render_seq_operator', text = "Render", icon='RENDER_ANIMATION')
+        footer = layout.row()
+        footer.operator('render.add_render', text = "Add")
+        footer.operator(RENDER_SEQ_OT.bl_idname, text = "Render", icon='RENDER_ANIMATION')
 
 class RENDER_OT_AddRender(bpy.types.Operator):
     """ Create a new render with its sensor(s) and/or effector(s) """
@@ -196,6 +303,7 @@ class RENDER_OT_AddRender(bpy.types.Operator):
 
     def execute(self, context):
         bpy.data.workspaces[0].render_panel_props.add()
+        print(f"Added render")
         return {'FINISHED'}
     
 class RENDER_OT_DeleteRender(bpy.types.Operator):
@@ -207,8 +315,25 @@ class RENDER_OT_DeleteRender(bpy.types.Operator):
 
     def execute(self, context):
         bpy.data.workspaces[0].render_panel_props.remove(self.index)
+        print(f"Removed {self.index} render")
         return {'FINISHED'}
+#duplicate render
+class RENDER_OT_DuplicateRender(bpy.types.Operator):
+    bl_idname = "render.duplicate_render"
+    bl_label = "Duplicate render"
+    index: bpy.props.IntProperty()
 
+    def execute(self, context):
+        initial = bpy.data.workspaces[0].render_panel_props[self.index]
+        duplicate = bpy.data.workspaces[0].render_panel_props.add()
+        #copy properties from initial to duplicate
+        for prop in initial.bl_rna.properties:
+            prop_name = prop.name
+            if hasattr(initial, prop_name):
+                setattr(duplicate, prop_name, getattr(initial, prop_name))
+        print(f"Duplicated {self.index} render")
+        return {'FINISHED'}
+    
 class RENDER_SEQ_OT(bpy.types.Operator):
     bl_idname = "render.render_seq_operator"
     bl_label = "Render Sequence Operator"
@@ -324,11 +449,13 @@ class RENDER_SEQ_OT(bpy.types.Operator):
                     frame_start = self.render_list[self.index].frame_start
                     frame_end = self.render_list[self.index].frame_end
                     output_path = self.render_list[self.index].output_path
+                    collection_visibility = self.render_list[self.index].collection_visibility
                     
                     if self.set_render_settings(rig_name, scene_name, cam_name, view_name, track_name, frame_start, frame_end, output_path) is False:
                         print("please check the render settings")
                         return {'FINISHED'}
-                    
+                    if collection_visibility and collection_visibility != '':
+                        bpy.ops.view3d.store_collection_visibility(index=self.index) #call restore collection vis operator
                     print(f"Currently rendering:{rig_name},{scene_name}, {cam_name}, {view_name}, {track_name}, {frame_start}, {frame_end}, {output_path}")
                     # Call another instance of render with animation
                     bpy.ops.render.render('INVOKE_DEFAULT', animation=True, write_still=True)
@@ -343,13 +470,16 @@ classes = (
             RENDER_PT,
             RENDER_OT_AddRender,
             RENDER_OT_DeleteRender,
-            RENDER_SEQ_OT
+            RENDER_OT_DuplicateRender,
+            RENDER_SEQ_OT,
+            STORE_OT_collection_visibility,
+            RESTORE_OT_collection_visibility,
+            CLEAR_OT_collection_visibility,
         )
         
 def register():
     for my_class in classes:
         bpy.utils.register_class(my_class)
-    # We only need a single collection property after all.
     bpy.types.WorkSpace.render_panel_props = bpy.props.CollectionProperty(type = RENDER_Props)
         
 def unregister():
