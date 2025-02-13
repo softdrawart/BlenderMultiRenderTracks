cc96ea2e66c0c3ec135ca307da9a52f3cc37e4b6
Mikhail Lebedev <softdrawart@gmail.com>
2025-02-13 14:45:11 +0300

    Update render_animation_sequence_v6.py
    update track time delete tracks add all tracks from active armature

--- render_animation_sequence_v6.py
+++ render_animation_sequence_v6.py
@@ -11,10 +11,21 @@ bl_info = {
 import bpy
 import json
 
+CHARACTER_NAME = "character"
+
 '''FIX 1. currently default scene is None, if it will be called none it will be ambigous whether its a scene or not
         2.  current value '0' matches no enum in 'RENDER_Props', '', 'track_name
         3. group in a way where scene 
-        4. Store Settings before changing them'''
+        4. Store Settings before changing them
+        5. remove solo from all tracks if any and remove if present Action from Active Action (edited action) (maybe first put a guard and then remove) turn off preview range if set
+        6. move UP/Down render blocks
+        7. copy settings from one to another with a button copy/paste
+        8. Fold all button
+        9. keep track name when rig selection is changed (if track name is set and new rig selection has same track, then keep it)
+        10. add groups for render blocks (this is useful when there are a lot of similar render blocks such as same character name etc.)
+        11. add button to automatically add all rig Tracks for the selected rig
+        12. add button to update all render blocks for track length change and output name (currently it updates when we change/select track) and button to enable all or disable all blocks
+        13. disable renders that are finished render or highlight the render that is currently rendering'''
 
 '''1. First we select the scene name
    2. then we select the view layer 
@@ -23,8 +34,8 @@ import json
 
 def get_collection_path(layer_collection, path=""):
     """ Recursively build the full path for a collection. """
-    path = f"{path}/{layer_collection.collection.name}" if path else layer_collection.collection.name
-    paths = {path: layer_collection.exclude}
+    path = f"{path}/{layer_collection.collection.name}" if path else layer_collection.collection.name #include path/collection name (first item will be just collection name) 
+    paths = {path: layer_collection.exclude} #collect exclude (collection visibility) (path: Boolean)
     for child in layer_collection.children:
         paths.update(get_collection_path(child, path))
     return paths
@@ -92,6 +103,7 @@ def update_view_list(self, context):
     return []
 
 def update_armature_list(self, context):
+    '''
     scene_data = bpy.data.scenes.get(self.scene_name)
     view_data = scene_data.view_layers.get(self.view_name) if scene_data else None
     if scene_data and view_data:
@@ -99,8 +111,11 @@ def update_armature_list(self, context):
         return armature_items
     
     return []
+    '''
+    return [(obj.name, obj.name, '') for obj in bpy.data.objects if obj.type == 'ARMATURE']
 
 def update_camera_list(self, context):
+    '''
     scene_data = bpy.data.scenes.get(self.scene_name)
     view_data = scene_data.view_layers.get(self.view_name) if scene_data else None
     if scene_data and view_data:
@@ -108,14 +123,22 @@ def update_camera_list(self, context):
         camera_items.insert(0, ("NONE", "None", "No camera selected"))  # Add empty option
         return camera_items
     return []
+    '''
+    camera_items = [(obj.name, obj.name, '') for obj in bpy.data.objects if obj.type == 'CAMERA']
+    camera_items.insert(0, ("NONE", "None", "No camera selected"))  # Add empty option
+    return camera_items
 
 def update_track_list(self, context):
-    view_data = bpy.data.scenes[self.scene_name].view_layers.get(self.view_name) if bpy.data.scenes.get(self.scene_name) else None
+    '''view_data = bpy.data.scenes[self.scene_name].view_layers.get(self.view_name) if bpy.data.scenes.get(self.scene_name) else None
     arm_data = view_data.objects.get(self.rig_name) if view_data else None
     if view_data and arm_data:
         if arm_data.animation_data and arm_data.animation_data.nla_tracks:
             track_items = [(trk.name, trk.name, '') for trk in arm_data.animation_data.nla_tracks] #check object not armature
             return track_items
+    return []'''
+    arm_data = bpy.data.objects.get(self.rig_name)
+    if arm_data and arm_data.type == 'ARMATURE' and arm_data.animation_data and arm_data.animation_data.nla_tracks:
+        return [(trk.name, trk.name, '') for trk in arm_data.animation_data.nla_tracks]
     return []
 
 def update_time(self, context):
@@ -161,6 +184,23 @@ def update_output_folder(self, context):
         output += f"{self.cam_name}\\"
         
     self.output_path = output
+
+def update_enable_all(self, context):
+    props = bpy.data.workspaces[0].render_panel_props
+    if props and len(props)>0:
+        for prop in props:
+            if prop.enabled != self.render_enable_all:
+                #copy enable from global enable boolean
+                prop.enabled = self.render_enable_all
+                print(f"Changed enable for {prop.track_name}")
+def update_folded_all(self, context):
+    props = bpy.data.workspaces[0].render_panel_props
+    if props and len(props)>0:
+        for prop in props:
+            if prop.folded != self.render_folded_all:
+                #copy enable from global enable boolean
+                prop.folded = self.render_folded_all
+                print(f"Folded {prop.track_name}")
 
 def form_render_text(self, property):
     #format "{character_name|track_name|camera}"
@@ -208,7 +248,7 @@ class RENDER_Props(bpy.types.PropertyGroup):
     )
     character_name: bpy.props.StringProperty(
         description="Name of the Character",
-        default="scene_name",
+        default=CHARACTER_NAME,
         update=update_output_folder
     )
     scene_name: bpy.props.EnumProperty(
@@ -252,8 +292,20 @@ class RENDER_PT(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
+        workspace = bpy.data.workspaces[0]
+        properties = workspace.render_panel_props
+        header = layout.row()
+        #Utils | character name that is used to add all tracks
+        header.prop(workspace, 'render_character_name')
+        header.operator(RENDER_OT_AddAllTracks.bl_idname, text = "Add all tracks")
+        header = layout.row()
+        #Global settings | update all tracks, fold or unfold all tracks, enable or disable tracks
+        if len(properties) > 0:
+            header.prop(workspace, 'render_enable_all')
+            header.operator(RENDER_OT_UpdateTrackTime.bl_idname, text = "Update Time")
+            header.operator(RENDER_OT_DeleteTracks.bl_idname, text = "Remove selected")
+            header.prop(workspace, "render_folded_all", icon='DISCLOSURE_TRI_DOWN' if workspace.render_folded_all else 'DISCLOSURE_TRI_RIGHT')
         # Here you want to iterate over your properties and keep the index for the Delete button
-        properties = bpy.data.workspaces[0].render_panel_props
         for i, prop in enumerate(properties):
             render_box = layout.box()
             header = render_box.row()
@@ -295,6 +347,84 @@ class RENDER_PT(bpy.types.Panel):
         footer = layout.row()
         footer.operator('render.add_render', text = "Add")
         footer.operator(RENDER_SEQ_OT.bl_idname, text = "Render", icon='RENDER_ANIMATION')
+
+class RENDER_OT_AddAllTracks(bpy.types.Operator):
+    """Add all NLA tracks from the active armature"""
+    bl_idname = "render.add_all_tracks"
+    bl_label = "Add All Tracks of Active Armature"
+
+    @classmethod
+    def poll(cls, context):
+        """Ensure an armature with NLA tracks is selected."""
+        arm = context.active_object
+        return (
+            arm 
+            and arm.type == 'ARMATURE' 
+            and arm.animation_data 
+            and len(arm.animation_data.nla_tracks) > 0
+        )
+
+    def execute(self, context):
+        workspace = bpy.data.workspaces[0]
+        props = workspace.render_panel_props  # Ensure this exists
+        arm = context.active_object  # Get the active armature
+        scene = context.scene.name
+        view = context.view_layer.name
+
+        existing_track_character_names = {prop.track_name: prop.character_name for prop in props}
+        character_name = workspace.render_character_name
+
+        if arm.animation_data and arm.animation_data.nla_tracks:
+            #collect all collection visibiltiy
+            collection_visibility = json.dumps(get_collection_path(bpy.context.view_layer.layer_collection))
+
+            for track in arm.animation_data.nla_tracks:
+                #if track name is in existing names and character name is not default CHARACTER_NAME
+                if (track.name not in existing_track_character_names) or (existing_track_character_names[track.name] != character_name):
+                    prop = props.add()
+                    prop.character_name = character_name
+                    prop.rig_name = arm.name
+                    prop.scene_name = scene
+                    prop.view_name = view
+                    prop.track_name = track.name
+                    prop.collection_visibility = collection_visibility
+                    print(f"Added track: {track.name}")
+
+        return {'FINISHED'}
+
+class RENDER_OT_UpdateTrackTime(bpy.types.Operator):
+    """ Update selected tracks"""
+    bl_idname = "render.update_track_time"
+    bl_label = "update selected tracks"
+
+    def execute(self, context):
+        props = bpy.data.workspaces[0].render_panel_props
+        if props and len(props)>0:
+            for prop in props:
+                if prop.enabled:
+                    if prop.track_name:
+                        #assign the same track name and it will call update frames function
+                        old_name = prop.track_name
+                        prop.track_name = old_name
+                        print(f"Updated Times {prop.track_name}")
+        return {'FINISHED'}
+
+class RENDER_OT_DeleteTracks(bpy.types.Operator):
+    """ delete selected tracks """
+    bl_idname = "render.delete_tracks"
+    bl_label = "delete selected tracks"
+
+    def execute(self, context):
+        props = bpy.data.workspaces[0].render_panel_props
+        #loop and delete only that prop that is enabled
+        if props and len(props) > 0:
+            for index in reversed(range(len(props))):  # Loop from last to first
+                prop = props[index]
+                if prop.enabled:
+                    track_name = prop.track_name
+                    props.remove(index)  # Safe removal
+                    print(f"{track_name} is removed")
+        return {'FINISHED'}
 
 class RENDER_OT_AddRender(bpy.types.Operator):
     """ Create a new render with its sensor(s) and/or effector(s) """
@@ -351,18 +481,22 @@ class RENDER_SEQ_OT(bpy.types.Operator):
         if scene is None:
             print(f"Scene '{scene_name}' not found!")
             return False
-        else:
-            bpy.context.window.scene = scene
+        
+        bpy.context.window.scene = scene
 
          # Get the view layer by name
         view = scene.view_layers.get(view_name)
         if view is None:
             print(f"View Layer '{view_name}' not found!")
             return False
-        else:
-            bpy.context.window.view_layer = view
-            
-
+        
+        bpy.context.window.view_layer = view
+        
+        #set collection
+        collection_visibility = self.render_list[self.index].collection_visibility
+        if collection_visibility and collection_visibility != '':
+            bpy.ops.view3d.restore_collection_visibility(index=self.index) #call restore collection vis operator'
+                    
         # Get the armature object
         armature_obj = bpy.data.objects.get(rig_name)
         if armature_obj is None or armature_obj.type != 'ARMATURE':
@@ -382,18 +516,16 @@ class RENDER_SEQ_OT(bpy.types.Operator):
         
         #set the camera active
         cam_obj = bpy.data.objects.get(cam_name)
-        if cam_obj is None or cam_obj.type != 'CAMERA':
-            print("No camera in the scene!")
-        else:
-            bpy.context.scene.camera = cam_obj
+        if cam_obj and cam_obj.type == 'CAMERA':
+            scene.camera = cam_obj
 
-        # Set the render frame range
-        bpy.context.scene.frame_start = frame_start
-        bpy.context.scene.frame_end = frame_end
-        #bpy.context.scene.cycles.samples = samples
-        bpy.context.scene.render.fps = 12
-        bpy.context.scene.render.use_file_extension = True
-        bpy.context.scene.render.image_settings.color_mode = 'RGBA'
+        # Set the render settings
+        scene.frame_start = frame_start
+        scene.frame_end = frame_end
+        scene.render.fps = 12
+        scene.render.use_file_extension = True
+        scene.render.image_settings.color_mode = 'RGBA'
+        scene.render.filepath = output_path
 
         # Set the render output path
         bpy.context.scene.render.filepath = output_path
@@ -411,6 +543,13 @@ class RENDER_SEQ_OT(bpy.types.Operator):
         self.stop = True
     
     def execute(self, context):
+        # Unregister old handlers if they exist
+        if self.pre in bpy.app.handlers.render_pre:
+            bpy.app.handlers.render_pre.remove(self.pre)
+        if self.complete in bpy.app.handlers.render_complete:
+            bpy.app.handlers.render_complete.remove(self.complete)
+        if self.canceled in bpy.app.handlers.render_cancel:
+            bpy.app.handlers.render_cancel.remove(self.canceled)
         #set default params
         self.index = 0
         self.stop = False
@@ -429,34 +568,41 @@ class RENDER_SEQ_OT(bpy.types.Operator):
         
     def modal(self, context, event):
         if event.type == 'TIMER':
-            if True in (self.index >= len(self.render_list), self.stop is True):
+            if self.index >= len(self.render_list) or self.stop:
                 #render is done or canceled
-                #removing all threads and timers
-                bpy.app.handlers.render_pre.remove(self.pre)
-                bpy.app.handlers.render_complete.remove(self.complete)
-                bpy.app.handlers.render_cancel.remove(self.canceled)
+                # Ensure handlers are removed before finishing
+                if self.pre in bpy.app.handlers.render_pre:
+                    bpy.app.handlers.render_pre.remove(self.pre)
+                if self.complete in bpy.app.handlers.render_complete:
+                    bpy.app.handlers.render_complete.remove(self.complete)
+                if self.canceled in bpy.app.handlers.render_cancel:
+                    bpy.app.handlers.render_cancel.remove(self.canceled)
+
                 bpy.context.window_manager.event_timer_remove(self._timer)
                 print("Nothing to Render!")
                 return {'FINISHED'}
-            elif self.rendering is False:
+            
+            elif not self.rendering:
                 if self.render_list[self.index].enabled:
                     #set the render settings and render
-                    rig_name = self.render_list[self.index].rig_name
-                    scene_name = self.render_list[self.index].scene_name
-                    cam_name = self.render_list[self.index].cam_name
-                    view_name = self.render_list[self.index].view_name
-                    track_name = self.render_list[self.index].track_name
-                    frame_start = self.render_list[self.index].frame_start
-                    frame_end = self.render_list[self.index].frame_end
-                    output_path = self.render_list[self.index].output_path
-                    collection_visibility = self.render_list[self.index].collection_visibility
+                    render_data = self.render_list[self.index]
+                    success = self.set_render_settings(
+                        render_data.rig_name,
+                        render_data.scene_name,
+                        render_data.cam_name,
+                        render_data.view_name,
+                        render_data.track_name,
+                        render_data.frame_start,
+                        render_data.frame_end,
+                        render_data.output_path
+                    )
                     
-                    if self.set_render_settings(rig_name, scene_name, cam_name, view_name, track_name, frame_start, frame_end, output_path) is False:
-                        print("please check the render settings")
+                    
+                    if not success:
+                        print("Please check the render settings")
                         return {'FINISHED'}
-                    if collection_visibility and collection_visibility != '':
-                        bpy.ops.view3d.store_collection_visibility(index=self.index) #call restore collection vis operator
-                    print(f"Currently rendering:{rig_name},{scene_name}, {cam_name}, {view_name}, {track_name}, {frame_start}, {frame_end}, {output_path}")
+                    
+                    print(f"Rendering: {render_data.rig_name}, {render_data.scene_name}, {render_data.cam_name}, {render_data.view_name}, {render_data.track_name}, {render_data.frame_start}, {render_data.frame_end}, {render_data.output_path}")
                     # Call another instance of render with animation
                     bpy.ops.render.render('INVOKE_DEFAULT', animation=True, write_still=True)
                 else:
@@ -471,6 +617,9 @@ classes = (
             RENDER_OT_AddRender,
             RENDER_OT_DeleteRender,
             RENDER_OT_DuplicateRender,
+            RENDER_OT_UpdateTrackTime,
+            RENDER_OT_DeleteTracks,
+            RENDER_OT_AddAllTracks,
             RENDER_SEQ_OT,
             STORE_OT_collection_visibility,
             RESTORE_OT_collection_visibility,
@@ -481,6 +630,21 @@ def register():
     for my_class in classes:
         bpy.utils.register_class(my_class)
     bpy.types.WorkSpace.render_panel_props = bpy.props.CollectionProperty(type = RENDER_Props)
+    bpy.types.WorkSpace.render_enable_all = bpy.props.BoolProperty(
+        name="",
+        default=False,
+        update=update_enable_all
+    )
+    bpy.types.WorkSpace.render_character_name = bpy.props.StringProperty(
+        name="character name",
+        default=CHARACTER_NAME
+    )
+    bpy.types.WorkSpace.render_folded_all = bpy.props.BoolProperty(
+        name="",
+        default=True,
+        update=update_folded_all
+    )
+    
         
 def unregister():
     for my_class in classes:
